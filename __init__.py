679f8a92a0337eb4235447901fef94dbaffd1ca9
Bennett Kanuka <bkanuka@gmail.com>
2020-03-30 07:13:33 -0400

    basic function

---
+++ __init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__license__   = 'GPL v3'
+__copyright__ = '2020, Bennett Kanuka <bkanuka@gmail.com>'
+__docformat__ = 'restructuredtext en'
+
+# The class that all Interface Action plugin wrappers must inherit from
+from calibre.customize import InterfaceActionBase
+
+class HideStatusbarPlugin(InterfaceActionBase):
+
+    name                = 'Hide Statusbar'
+    description         = 'Hides the statusbar'
+    supported_platforms = ['windows', 'osx', 'linux']
+    author              = 'Bennett Kanuka'
+    version             = (1, 0, 0)
+    minimum_calibre_version = (0, 7, 53)
+
+    #: This field defines the GUI plugin class that contains all the code
+    #: that actually does something. Its format is module_path:class_name
+    #: The specified class must be defined in the specified module.
+    actual_plugin       = 'calibre_plugins.hide_statusbar.ui:InterfacePlugin'
+
+    def is_customizable(self):
+        return False
+
---
+++ ui.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__license__   = 'GPL v3'
+__copyright__ = '2020, Bennett Kanuka <bkanuka@gmail.com>'
+__docformat__ = 'restructuredtext en'
+
+
+# The class that all interface action plugins must inherit from
+from calibre.gui2.actions import InterfaceAction
+
+class InterfacePlugin(InterfaceAction):
+
+    name = 'Hide Statusbar'
+
+    def gui_layout_complete(self):
+        self.gui.status_bar.hide()
+
+
